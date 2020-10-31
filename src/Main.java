456ad9ee1d55c299aedce5b319c827f94ed7086f
milden6 <milden6@gmail.com>
2020-10-31 21:50:55 +0300

    Removed all fiels

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-
-        CharactersGenerator generator = new CharactersGenerator();
-        System.out.println(generator.GeneratePassword(32, true, false, true));
-    }
-}
